e68472107935f035b915eb76daf5b37228676db1
reidrac <jjm@usebox.net>
2012-12-16 13:59:18 +0000

    warn the user if secrets.conf is world readable

--- swiftnbd/common.py
+++ swiftnbd/common.py
@@ -24,6 +24,7 @@ THE SOFTWARE.
 
 import logging
 import errno
+import os
 from cStringIO import StringIO
 from hashlib import md5
 from ConfigParser import RawConfigParser
@@ -32,6 +33,11 @@ from swiftclient import client
 
 def getSecrets(container, secrets_file):
     """Read secrets"""
+
+    stat = os.stat(secrets_file)
+    if stat.st_mode & 0x004 != 0:
+        log = logging.getLogger(__package__)
+        log.warn("%s is world readable, please consider changing its permissions to 0600" % secrets_file)
 
     conf = RawConfigParser(dict(username=None, password=None))
     conf.read(secrets_file)
