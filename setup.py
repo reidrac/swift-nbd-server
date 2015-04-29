e9c197b1672d4321af1ea37a89edb37fba46eca8
Juan J. Martinez <jjm@usebox.net>
2015-04-29 21:28:10 +0100

    removed gevent, added asyncio

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(name="swiftnbd",
       license="MIT",
       include_package_data=True,
       zip_safe=False,
-      install_requires=["python-swiftclient>=1.2.0", "gevent>=1.0"],
+      install_requires=["python-swiftclient>=1.2.0", "asyncio"],
       scripts=["bin/swiftnbd-server", "bin/swiftnbd-ctl"],
       packages=find_packages(exclude=["tests"]),
       classifiers=[
