9fcdcf2ba73dfafd3c350793f923c0acedb65892
Juan J. Martinez <jjm@usebox.net>
2015-04-29 21:28:21 +0100

    ready for release 0.10.1

--- swiftnbd/const.py
+++ swiftnbd/const.py
@@ -21,7 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 """
-version = "0.10"
+version = "0.10.1"
 description = "This is a NBD server for OpenStack Object Storage (Swift)."
 project_url = "https://github.com/reidrac/swift-nbd-server"
 
